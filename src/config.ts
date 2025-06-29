0ef91c33b26399c5f7c92b9530c5d8d913e67cae
Benjamin Roth <benjamin.roth@jaumo.com>
2025-06-29 14:18:41 +0200

    Debounce + filter old events

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config'
-import { cleanEnv, host, port, str } from 'envalid'
+import { cleanEnv, host, num, port, str } from 'envalid'
 
 export default cleanEnv(
     // eslint-disable-next-line no-process-env
@@ -12,6 +12,14 @@ export default cleanEnv(
         MQTT_PASSWORD: str(),
         MQTT_PORT: port({ default: 1883 }),
         MQTT_HOST: str(),
+        PASS_DEBOUNCE: num({
+            default: 5000,
+            desc: 'Skip door opening messages if last open was less then milliseconds ago',
+        }),
+        PASS_MAX_DELAY: num({
+            default: 10000,
+            desc: 'Skip door opening messages if message is older than milliseconds',
+        }),
         EVENT_TOPIC: str({ default: 'access_control/events/' }),
         IMAGE_TOPIC: str({ default: 'access_control/image/' }),
         ATTEMPT_TOPIC: str({ default: 'access_control/attempt/' }),
